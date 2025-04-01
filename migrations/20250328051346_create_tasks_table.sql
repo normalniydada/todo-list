a20cd425ee426fa5a2f8cf30539fba2914315906
juggernaut <normalniydadawade@gmail.com>
2025-04-01 11:43:25 +0700

    fixed

--- migrations/20250328051346_create_tasks_table.sql
+++ migrations/20250328051346_create_tasks_table.sql
@@ -1,12 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE tasks (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY,
     title TEXT NOT NULL,
-    content TEXT NOT NULL,
-    done BOOLEAN NOT NULL DEFAULT FALSE,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
+    content TEXT NOT NULL
 );
 -- +goose StatementEnd
 
